e5b128a47b6488873a69ac5c688438bfbf8d6e15
cntt3-hueha <huehathi710@gmail.com>
2020-03-28 12:18:11 +0700

    update main.js

--- main.js
+++ main.js
@@ -27,8 +27,8 @@ function randomAge(a, b) {
 
 for(let i = 0; i < 40; i++) {
     let id = Date.now() + ( (Math.random()*100000).toFixed());
-    let fullname = randomItem(firstNames) + ' ' + randomItem(lastNames);
-    
+    let firstname = randomItem(firstNames); 
+    let lastname = randomItem(lastNames);
     let gender = randomItem(genders);
     let age = randomAge(20,40);
     let birthDay = randomDate(new Date(1980, 0, 1), new Date());
@@ -36,7 +36,8 @@ for(let i = 0; i < 40; i++) {
     
     let user = {
         'id': id,
-        'fullname': fullname,
+        'firstname': firstname,
+        'lastname': lastname,
         'age': age,
         'gender': gender,
         'birthday': birthDay,
@@ -46,4 +47,34 @@ for(let i = 0; i < 40; i++) {
 }
 
 console.log(users);
+// Change the first name of the user at position 3 to Smith
+    let Location = users[2];
+    console.log(Location);
+    Location.firstname = "Smith";
+    console.log(Location);
 
+
+//List all people in the list has full name (first name + last name) contains “an” (case insensitive)?
+let fullname = users.filter((f) => {
+        return (f.firstname === "An" ) || (f.lastname === "An" )
+    })
+console.log(fullname);
+
+
+//List all people in the list aged from 25 to 33, 38 and gender is male?
+
+var aged = users.filter((f) =>  (f.age === 38 && f.gender === "male") ||
+ ( (f.age >= 25) && (f.age <=33) &&(f.gender ==='male') ))
+console.log(aged);
+
+//  List all the people in the list has first name start with “s” and end with “h”. (Use regular expression)
+var useFirstname = users.filter(function(f){
+    const str = f.firstname;
+    return (str.toLowerCase().charAt(0)==='s' && str.toLowerCase().charAt(str.length - 1)==='h')
+}) ;
+console.log("useFirstname",useFirstname );
+
+//Remove all the people aged greater than 35
+
+
+//Add new 2 random users to first/last in current list (one first, one last)
